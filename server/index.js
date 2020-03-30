6a0600c98e253b6bfac8c95c290a602fd8c74341
Joaquin Noguera <nvjoaquien@gmail.com>
2020-03-29 21:25:03 -0300

    se agrego la dependencia cors al servidor

--- server/index.js
+++ server/index.js
@@ -7,7 +7,8 @@ const Country = require('./country');
 
 const app = express();
 app.use(express.json());
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
+
 
 app.use(cors());
 connect();
